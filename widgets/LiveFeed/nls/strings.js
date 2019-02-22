472271ba1ca895a625a1ddbca5a9891af7983ba9
Dzenan Dumpor <dzenan.dumpor@geodata.no>
2019-02-22 11:27:41 +0100

    add status placeholder

--- widgets/LiveFeed/nls/strings.js
+++ widgets/LiveFeed/nls/strings.js
@@ -1,8 +1,9 @@
 define({
   root: {
-    _widgetLabel: 'Live Feed',
-    widgetTitle: 'Live Feed',
-    description: 'Widget som viser alle kjøretøy med live posisjonering i kart'
+    _widgetLabel: "Live Feed",
+    widgetTitle: "Live Feed",
+    description: "Widget som viser alle kjøretøy med live posisjonering i kart",
+    serviceStatusTitle: "Status på tjenesten"
   }
   // add supported locales below:
   // , "zh-cn": true

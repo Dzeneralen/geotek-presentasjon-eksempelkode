a802b2cd6622b366d13763e72dfe4f34e85be646
Dzenan Dumpor <dumpor@outlook.com>
2019-02-26 18:17:40 +0100

    minimum viable widget added

---
+++ widgets/SimpleWidget/Widget.js
@@ -0,0 +1,40 @@
+define(["dojo/_base/declare", "jimu/BaseWidget"], function(
+  declare,
+  BaseWidget
+) {
+  return declare([BaseWidget], {
+    // postCreate: function() {
+    //   this.inherited(arguments);
+    //   console.log("SimpleWidget::postCreate");
+    // },
+    // startup: function() {
+    //   this.inherited(arguments);
+    //   console.log("SimpleWidget::startup");
+    // },
+    // onOpen: function() {
+    //   console.log("SimpleWidget::onOpen");
+    // },
+    // onClose: function() {
+    //   console.log("SimpleWidget::onClose");
+    // }
+    // onMinimize: function(){
+    //   console.log('SimpleWidget::onMinimize');
+    // },
+    // onMaximize: function(){
+    //   console.log('SimpleWidget::onMaximize');
+    // },
+    // onSignIn: function(credential){
+    //   console.log('SimpleWidget::onSignIn', credential);
+    // },
+    // onSignOut: function(){
+    //   console.log('SimpleWidget::onSignOut');
+    // }
+    // onPositionChange: function(){
+    //   console.log('SimpleWidget::onPositionChange');
+    // },
+    // resize: function(){
+    //   console.log('SimpleWidget::resize');
+    // }
+    //methods to communication between widgets:
+  });
+});
